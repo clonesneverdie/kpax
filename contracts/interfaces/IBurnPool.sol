82a0193f2a207abf0494fc32e7c46cf8f5a9ba58
Hanul <hanul@hanul.me>
2021-12-14 21:59:11 +0900

    init

--- contracts/interfaces/IBurnPool.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.5.6;
-
-interface IBurnPool {
-    function pid() external view returns (uint256);
-    function burn() external;
-}
--- contracts/klayswap/interfaces/IKLAYSwap.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity 0.5.6;
-
-interface IKLAYSwap {
-    function exchangeKlayPos(address token, uint amount, address[] calldata path) external payable;
-    function exchangeKctPos(address tokenA, uint amountA, address tokenB, uint amountB, address[] calldata path) external;
-}
